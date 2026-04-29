b78ff2b50fb8a3996b3e28e9025cd26638e6f153
kasyifana <muhammadli1009@gmail.com>
2026-04-29 13:18:02 +0700

    fix: add trustHost to authConfig for Vercel deployment

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -2,6 +2,7 @@ import type { NextAuthConfig } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 
 export const authConfig = {
+  trustHost: true,
   providers: [
     Credentials({
       credentials: {
